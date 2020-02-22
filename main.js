28f327d071d0da1919f23a9efed4251cba64c137
Olamide Oladinni <olajide1987@gmail.com>
2020-02-22 12:53:57 -0600

    update

--- main.js
+++ main.js
@@ -34,44 +34,71 @@ const dinos = [
     }
   ];
   
+  const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+  };
   
-const printToDom = (DivId, TextToPrint) => {
-    const selectedDIv = document.getElementById(DivId);
-    selectedDIv.innerHTML = TextToPrint;
-};
-
-const closeSingleViewEvent = () => {
+  const closeSingleViewEvent = () => {
     printToDom('single-view', '');
     printDinos(dinos);
-};
-
-
-const viewSingleDino = () => {
-    let domString = ''
+  };
+  
+  // const find = (dinoId) => {
+  //   for(let i =0; i < dinos.length; i++){
+  //     if(dinoId === dinos[i].id){
+  //       return dinos[i];
+  //     }
+  //   }
+  // }
+  
+  const viewSingleDino = (e) => {
+    const dinoId = e.target.closest('.card').id;
+    // const selectedDino = find(dinoId);
+    const selectedDino = dinos.find((x) => dinoId === x.id);
+    let domString = '';
     domString += '<button id="close-single-view" class="btn btn-outline-dark"><i class="far fa-times-circle"></i></button>';
+    domString += '<div class="container">';
+    domString += '<div class="row">';
+    domString += '<div class="col-6">';
+    domString += `<img class="img-fluid" src="${selectedDino.imageUrl}" alt=""/>`;
+    domString += '</div>';
+    domString += '<div class="col-6">';
+    domString += `<h2>${selectedDino.name}</h2>`;
+    domString += `<p>Type: ${selectedDino.type}</p>`;
+    domString += `<p>Age: ${selectedDino.age}</p>`;
+    domString += `<p>Owner: ${selectedDino.owner}</p>`;
+    domString += `<p>Health: ${selectedDino.health}</p>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
+  
+  
+  
+  
+  
     printToDom('kennel', '');
     printToDom('single-view', domString);
     document.getElementById('close-single-view').addEventListener('click', closeSingleViewEvent);
   };
-
-
+  
   const singleDinoAddEvents = () => {
     const dinoViewButtons = document.getElementsByClassName('single-dino');
     for(let i = 0; i < dinoViewButtons.length; i++){
       dinoViewButtons[i].addEventListener('click', viewSingleDino);
     }
   };
-
-const printDinos = (dinoArray) => {
+  
+  const printDinos = (dinoArray) => {
     let domString = '';
     for (let i =0; i < dinoArray.length; i++){
       domString += '<div class="col-4">';
-      domString += '<div class="card">';
+      domString += `<div id="${dinoArray[i].id}" class="card">`;
       domString += `<img class="card-img-top" src="${dinoArray[i].imageUrl}" alt="Card image cap">`;
       domString += '<div class="card-body">';
-      domString += `  <h5 class="card-title">${dinoArray[i].name}</h5>`;
-      domString += `  <p class="card-text">Health: ${dinoArray[i].health}</p>`;
-      domString += '<button class="btn btn-outline-dark single-dino"><i class="far fa-eye"></i></i></button>';
+      domString += `<h5 class="card-title">${dinoArray[i].name}</h5>`;
+      domString += `<p class="card-text">Health: ${dinoArray[i].health}</p>`;
+      domString += '<button class="btn btn-outline-dark single-dino"><i class="far fa-eye"></i></button>'
       domString += '</div>';
       domString += '</div>';
       domString += '</div>';
@@ -79,29 +106,30 @@ const printDinos = (dinoArray) => {
     printToDom('kennel', domString);
     singleDinoAddEvents();
   };
-
-const newDino = (e) => {
+  
+  
+  
+  const newDino = (e) => {
     e.preventDefault();
-    const brandNewDino = {
-    id: `dino${dinos.length + 1}`,
-    name: document.getElementById('dino-name').value,
-    type: document.getElementById('dino-type').value,
-    age: document.getElementById('dino-age').value,
-    owner: document.getElementById('dino-owner').value,
-    adventures: [],
-    health: 100,
-    imageUrl: document.getElementById('dino-image').value
+    const brandNewDino =   {
+      id: `dino${dinos.length + 1}`,
+      name: document.getElementById('dino-name').value,
+      type: document.getElementById('dino-type').value,
+      age: document.getElementById('dino-age').value,
+      owner: document.getElementById('dino-owner').value,
+      adventures: [],
+      health: 100,
+      imageUrl: document.getElementById('dino-image').value
     };
     dinos.push(brandNewDino);
     document.getElementById('new-dino-form').reset();
     document.getElementById('collapseOne').classList.remove('show');
     printDinos(dinos);
-    
-};
-
-const init = () => {
-    document.getElementById('submit-new-dino').addEventListener('click',newDino)
+  };
+  
+  const init = () => {
+    document.getElementById('submit-new-dino').addEventListener('click', newDino);
     printDinos(dinos);
-};
-
-init();
+  };
+  
+  init();
