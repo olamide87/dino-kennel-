f0d839a57dcf5618b2d628d06d7d8fd62f07ad64
Olamide Oladinni <olajide1987@gmail.com>
2020-02-22 11:00:28 -0600

    dom

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 
 
 
-const dinos = [4];
+const dinos = [];
 
+const printToDom = (DivId, TextToPrint) => {
+    const selectedDIv = document.getElementById(DivId);
+    selectedDIv.innerHTML = TextToPrint;
+};
 
-
+const printDinos = (dinoArray) => {
+    let domString = '';
+    for (let i =0; i < dinoArray.length; i++){
+      domString += '<div class="col-4">';
+      domString += '<div class="card">';
+      domString += `<img class="card-img-top" src="${dinoArray[i].imageUrl}" alt="Card image cap">`;
+      domString += '<div class="card-body">';
+      domString += `  <h5 class="card-title">${dinoArray[i].name}</h5>`;
+      domString += `  <p class="card-text">Health: ${dinoArray[i].health}</p>`;
+      domString += '</div>';
+      domString += '</div>';
+      domString += '</div>';
+    }
+    printToDom('kennel', domString);
+  };
 
 const newDino = (e) => {
     e.preventDefault();
@@ -21,8 +39,9 @@ const newDino = (e) => {
     dinos.push(brandNewDino);
     document.getElementById('new-dino-form').reset();
     document.getElementById('collapseOne').classList.remove('show');
-    console.log('hi from newDIno' , dinos);
-}
+    printDinos(dinos);
+    
+};
 
 const init = () => {
     document.getElementById('submit-new-dino').addEventListener('click',newDino)
